8e7005413d7bfa5e3001be64cb0d3151417c58ac
Ersen <wuersen@hotmail.com>
2021-01-05 22:30:04 +1100

    Tue5

--- js/project0.js
+++ js/project0.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
 
 // $('.line1,.line2,.line3,.display').css('opacity',0);
-$('.line1,.display').fadeTo(1000,0.9)
-$('.line2').fadeTo(2000,0.9)
-$('.line3').fadeTo(2500,0.9)
-$('h1').fadeTo(1000,0.9)
-$('.message').fadeTo(3000, 0.9)
+$('.line1,.display').fadeTo(1000,0.8)
+$('.line2').fadeTo(2000,0.8)
+$('.line3').fadeTo(2500,0.8)
+$('h1').fadeTo(1000,0.8)
+$('.message').fadeTo(3000, 0.8)
 
 
 
@@ -36,28 +36,28 @@ let countTo9 = 0;
         $(`#line${i}Box${j}`).text()===$(`#line${i}Box${j+2}`).text()&&
         $(`#line${i}Box${j}`).text()!==''){
         $(`#line${i}Box${j}, #line${i}Box${j+1}, #line${i}Box${j+2}`).css('background-color','orange');
-        $('.message').text(`Player${$(this).text()}  wins`);
+        $('.message').text(`Player ${$(this).text()}  wins`);
         }
 //winning by having 3 in a column
     if ($(`#line${i}Box${j}`).text()===$(`#line${i+1}Box${j}`).text()&&
         $(`#line${i}Box${j}`).text()===$(`#line${i+2}Box${j}`).text()&&
         $(`#line${i}Box${j}`).text()!==''){
         $(`#line${i}Box${j}, #line${i+1}Box${j}, #line${i+2}Box${j}`).css('background-color','orange');
-        $('.message').text(`Player${$(this).text()} wins`);
+        $('.message').text(`Player ${$(this).text()} wins`);
         }
 //winning by having 3 in a line from top left to bottom right
     if ($(`#line${i}Box${j}`).text()===$(`#line${i+1}Box${j+1}`).text()&&
         $(`#line${i+1}Box${j+1}`).text()===$(`#line${i+2}Box${j+2}`).text()&&
         $(`#line${i}Box${j}`).text()!==''){
         $(`#line${i}Box${j}, #line${i+1}Box${j+1}, #line${i+2}Box${j+2}`).css('background-color','orange');
-        $('.message').text(`Player${$(this).text()} wins`);
+        $('.message').text(`Player ${$(this).text()} wins`);
         }
 //winning by having 3 in a line from bottm left to top right
     if ($(`#line${i}Box${j}`).text()===$(`#line${i-1}Box${j+1}`).text()&&
         $(`#line${i}Box${j}`).text()===$(`#line${i+1}Box${j-1}`).text()&&
         $(`#line${i}Box${j}`).text()!==''){
         $(`#line${i}Box${j}, #line${i-1}Box${j+1}, #line${i+1}Box${j-1}`).css('background-color','orange');
-        $('.message').text(`Player${$(this).text()} wins`);
+        $('.message').text(`Player ${$(this).text()} wins`);
         }
 
     }//end of inner for-loop
